9e37ee15f7758e7a791ce3d70dbdebcb970cef10
Akira Terui <hanabi0111@gmail.com>
2023-11-30 14:46:04 -0500

    edit: produce figure pdf

--- code/figure_test.R
+++ code/figure_test.R
@@ -5,83 +5,41 @@ source("code/library.R")
 source("code/function.R")
 
 df_sim <- readRDS("output/simulation.rds") %>% 
-  mutate(stability = ifelse(eigen_max > 1, "unstable", "stable"))
+  mutate(stability = ifelse(eigen_max > 1, "Sensitive", "Insensitive"))
 
 df_plot <- df_sim %>% 
   drop_na(eigen_max)
 
 # plot --------------------------------------------------------------------
 
-g1 <- df_plot %>% 
-  filter(sd_r == 0) %>% 
-  ggplot(aes(x = stability,
-             y = p)) +
-  geom_violin(aes(fill = stability),
-              alpha = 0.1,
-              draw_quantiles = 0.5) +
-  geom_jitter(height = 0,
-              width = 0.1,
-              alpha = 0.25,
-              aes(color = eigen_max)) +
-  facet_grid(rows = vars(n_timestep, factor_sd_a1),
-             cols = vars(n_species),
-             labeller = label_both)
-  
+label <- c('10' = 'Time series = 10',
+           '30' = 'Time series = 30',
+           '5' = "Species number = 5",
+           '15' = "Species number = 15")
 
-g2 <- df_plot %>% 
-  filter(sd_r == 0.1) %>% 
-  ggplot(aes(x = stability,
-             y = p)) +
-  geom_violin(aes(fill = stability),
-              alpha = 0.1,
-              draw_quantiles = 0.5) +
-  geom_jitter(height = 0,
-              width = 0.1,
-              alpha = 0.25) +
-  facet_grid(rows = vars(n_timestep, factor_sd_a1),
-             cols = vars(n_species),
-             labeller = label_both)
-
-g1 | g2
-
-
-w1 <- df_plot %>% 
-  filter(sd_r == 0) %>% 
+w <- df_plot %>% 
   ggplot(aes(x = eigen_max,
              y = p,
              color = stability)) +
-  geom_point(alpha = 0.25) +
+  geom_point(alpha = 0.25,
+             size = 0.5) +
   scale_x_continuous(trans = "log10") +
-  facet_wrap(facets = ~ n_species + n_timestep + factor_sd_a1,
-             #rows = vars(n_timestep, factor_sd_a1),
-             #cols = vars(n_species),
-             labeller = label_both,
-             ncol = 2,
-             nrow = 4,
-             scales = "free_x")
+  facet_grid(rows = vars(n_timestep),
+             cols = vars(n_species),
+             labeller = as_labeller(label),
+             scales = "free_x") +
+  coord_cartesian(xlim = c(0.5, 10)) +
+  labs(x = expression("Leading eigen value"~"|"*lambda[max]*"|"),
+       y = expression("Pr("*beta[obs]~">"~beta[null]*")"),
+       color = "Sensitivity") +
+  MetBrewer::scale_color_met_d("Hiroshige",
+                               direction = -1) +
+  theme_bw() +
+  theme(strip.background = element_blank(),
+        panel.grid = element_blank(),
+        legend.position = "bottom")
 
-# g1 <- df_plot %>%
-#   filter(sd_r == 0) %>%
-#   ggplot(aes(x = factor(factor_a1),
-#              y = p,
-#              color = stability)) +
-#   geom_boxplot() +
-#   geom_jitter(width = 0.1) +
-#   facet_grid(rows = vars(n_timestep, factor_sd_a1),
-#              cols = vars(n_species),
-#              labeller = label_both) +
-#   labs(x = "Relative strength of interspecific competition",
-#        y = "Pr(b > b_0)")
-# 
-# g2 <- df_plot %>% 
-#   filter(sd_r == 0.25) %>% 
-#   ggplot(aes(x = factor(factor_a1),
-#              y = p)) +
-#   geom_boxplot() +
-#   geom_jitter(width = 0.1) +
-#   facet_grid(rows = vars(n_timestep, factor_sd_a1),
-#              cols = vars(n_species),
-#              labeller = label_both) +
-#   labs(x = "Relative strength of interspecific competition",
-#        y = "Pr(b > b_0)",
-#        color = "SD in r")
+ggsave(w,
+       filename = "output/figure_eigen.pdf",
+       height = 3.5,
+       width = 3.5)
